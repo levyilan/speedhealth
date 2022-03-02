bbdf8e5e2f3b1828ffde7895c3a4dd62f5a1e083
levyilan <16539237+levyilan@users.noreply.github.com>
2022-03-02 02:54:57 +0200

    Update speedhealth.sh

--- speedhealth.sh
+++ speedhealth.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+chat_id="" # telegram chat id
+bot_id="" # telegram bot id
+bot_name="myuptimebot" # telegram bot name
+telegram_base_url="https://api.telegram.org/bot${bot_id}/sendMessage"
+speedtest_server="14215" # speed test server id
+speed_trashold=100 # speed trashold in mbits
+test_server_url="" # your server's url
+
+speed=`curl -sL ${telegram_base_url}/api/speedtest/latest | jq -M -r '.data.download'`
+if (( $(echo "${speed} < ${speed_trashold}" | bc -l) )); then
+    curl -X POST ${telegram_base_url} -d "chat_id=${chat_id}&text=Current Speed is ${speed}"
+fi
